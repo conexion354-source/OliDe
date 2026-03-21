01f8ce6cb2eeecd74957ae262b18d5c3cfc21ae3
Mauro <conexion354@gmail.com>
2026-03-21 00:37:38 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+
+const CACHE = "olide-pro-v2-20260321";
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
