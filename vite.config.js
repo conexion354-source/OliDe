8f40833efbcb495e52c65dc2e425a3814e02290a
Mauro <conexion354@gmail.com>
2026-04-17 14:21:23 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
+  // Esta línea le dice a Vite que tu app vive en /OliDe/ y no en la raíz
+  base: '/OliDe/', 
 })
