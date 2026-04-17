126e1c0439052c3cd58693b19dbfa9aaf75389d7
Mauro <conexion354@gmail.com>
2026-04-17 14:35:16 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* El basename evita que al hacer clic en un link te mande a tu otra app */}
-    <BrowserRouter basename="/OliDe">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
